7269ca54a09c663f9ee10f54eeee439023a3da87
MichaelAbe <michael.k.abe@gmail.com>
2021-02-02 13:29:27 -0500

    Show page display stylist name

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,11 @@ class Appointment < ApplicationRecord
     validates_presence_of :name, :appointment_date
     belongs_to :employee
 
-    #accepts_nested_attributes_for :employee, reject_if: :all_blank
+    accepts_nested_attributes_for :employee, reject_if: :all_blank
+
+    def employee_first_name
+        self.employee.first_name if self.employee
+    end
 end
 
 
