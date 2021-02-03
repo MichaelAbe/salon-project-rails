d57e9164bc92f5a00f992d169a469f6ca768fb99
MichaelAbe <michael.k.abe@gmail.com>
2021-02-03 10:22:33 -0500

    fixed error in config/routes.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
     def new #get/login
-
+        
     end
 
     def create #post/login
--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,9 @@ Rails.application.routes.draw do
 
   get "/signup", to: "users#new", as: "signup"
   post "/signup", to: "users#create"
-  get "/login", to: "sessions#new", as: "signup"
-  post "/login", to: "sessions#create
+  get "/login", to: "sessions#new", as: "login"
+  post "/login", to: "sessions#create"
 
   delete "/logout", to: "sessions#destroy", as: "destroy_user_session"
 
 end
-
-
-
